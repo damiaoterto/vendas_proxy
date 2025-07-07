cfad9a8969e0938fc019adce75d681ba1a561bcc
Francisco Damião Terto <contato@damiaoterto.dev>
2025-07-07 14:36:38 -0300

    feat: create proxy handler method

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/damiaoterto/vendas-proxy/internal/core"
+)
+
 func main() {
-	// TODO
+	proxy := core.NewProxy("0.0.0.0", 8080)
+	if err := proxy.Listen(); err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/core/proxy.go
+++ internal/core/proxy.go
@@ -1,4 +1,46 @@
 package core
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 type Proxy struct {
+	mux  *http.ServeMux
+	addr string
+	port int
+}
+
+func NewProxy(addr string, port int) *Proxy {
+	mux := http.NewServeMux()
+	return &Proxy{addr: addr, port: port, mux: mux}
+}
+
+func (p Proxy) proxyHandler(w http.ResponseWriter, r *http.Request) {
+	method := strings.ToLower(r.Method)
+	if method != "get" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	hostParts := strings.Split(r.Host, ".")
+	if len(hostParts) < 2 {
+		http.Error(w, "Invalid host", http.StatusBadRequest)
+		return
+	}
+	subdomain := hostParts[0]
+
+	fmt.Println(subdomain)
+}
+func (p *Proxy) Listen() error {
+	p.mux.HandleFunc("/", p.proxyHandler)
+
+	addr := fmt.Sprintf("%s:%d", p.addr, p.port)
+
+	if err := http.ListenAndServe(addr, p.mux); err != nil {
+		return fmt.Errorf("error on start http server: %v", err)
+	}
+
+	return nil
 }
